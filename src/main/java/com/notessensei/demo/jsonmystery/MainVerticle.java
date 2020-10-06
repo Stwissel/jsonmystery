f69f0293387cf3234e93f529cbf7acace73a3edb
Stephan W <stw@linux.com>
2020-10-06 22:43:27 +0800

    Complete show case

--- src/main/java/com/notessensei/demo/jsonmystery/MainVerticle.java
+++ src/main/java/com/notessensei/demo/jsonmystery/MainVerticle.java
@@ -1,23 +1,113 @@
 package com.notessensei.demo.jsonmystery;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import com.google.common.io.Resources;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.pointer.JsonPointer;
+import io.vertx.ext.json.schema.Schema;
+import io.vertx.ext.json.schema.SchemaParser;
+import io.vertx.ext.json.schema.SchemaRouter;
+import io.vertx.ext.json.schema.SchemaRouterOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
+  public static void main(final String[] args) {
+    final MainVerticle verticle = new MainVerticle();
+    final VertxOptions options = new VertxOptions()
+        .setBlockedThreadCheckIntervalUnit(TimeUnit.MINUTES)
+        .setBlockedThreadCheckInterval(10);
+    final Vertx vertx = Vertx.vertx(options);
+    vertx.deployVerticle(verticle)
+        .onFailure(Throwable::printStackTrace)
+        .onSuccess(v -> System.out.println("All done"));
+  }
+
+  private JsonObject openAPI;
+  private SchemaRouter schemaRouter;
+  private SchemaParser schemaParser;
+
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Please post to /roadlayouts");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
+  public void start(final Promise<Void> startPromise) throws Exception {
+
+    this.loadSchemaStuff()
+        .compose(v -> this.startWebServer())
+        .onSuccess(v -> startPromise.complete())
+        .onFailure(err -> startPromise.fail(err));
+
+  }
+
+  public Future<Void> startWebServer() {
+    return Future.future(startPromise -> {
+      final Router router = Router.router(this.getVertx());
+      router.route().handler(BodyHandler.create(false));
+      router.route(HttpMethod.GET, "/").handler(this::handleRoot);
+      router.route(HttpMethod.POST, "/trafficlight").handler(this::handleTrafficLight);
+      router.route(HttpMethod.POST, "/street").handler(this::handleStreet);
+
+      this.getVertx().createHttpServer()
+          .requestHandler(router)
+          .listen(8888)
+          .onSuccess(v -> {
+            System.out.println("HTTP Running on 8888");
+            startPromise.complete();
+          })
+          .onFailure(err -> startPromise.fail(err));
+    });
+  }
+
+  Schema getSchema(final String pointerString) {
+    final JsonPointer pointer = JsonPointer.from(pointerString);
+    final JsonObject schemaJson = (JsonObject) pointer.queryJson(this.openAPI);
+    return this.schemaParser.parse(schemaJson);
+  }
+
+  void handleRoot(final RoutingContext ctx) {
+    ctx.response().end("Hi there, please post to /street or /trafficlight");
+  }
+
+  void handleStreet(final RoutingContext ctx) {
+    final JsonObject body = ctx.getBodyAsJson();
+    final Schema schema = this.getSchema("/components/schemas/RoadLayout");
+    schema.validateAsync(body)
+        .onSuccess(v -> ctx.response().end("The submitted road is fine!"))
+        .onFailure(e -> ctx.response().setStatusCode(500).end(e.getMessage()));
+  }
+
+  void handleTrafficLight(final RoutingContext ctx) {
+    final JsonObject body = ctx.getBodyAsJson();
+    final Schema schema = this.getSchema("/components/schemas/TrafficLight");
+    schema.validateAsync(body)
+        .onSuccess(v -> ctx.response().end("The submitted traffic light is complete!"))
+        .onFailure(e -> ctx.response().setStatusCode(500).end(e.getMessage()));
+  }
+
+  Future<Void> loadSchemaStuff() {
+    return Future.future(promise -> {
+
+      final URL url = this.getClass().getResource("/SchemaDemo.json");
+      String openAPIString;
+      try {
+        openAPIString = Resources.toString(url, StandardCharsets.UTF_8);
+        this.openAPI = new JsonObject(openAPIString);
+        final SchemaRouterOptions so = new SchemaRouterOptions();
+        this.schemaRouter = SchemaRouter.create(this.getVertx(), so);
+        this.schemaParser = SchemaParser.createOpenAPI3SchemaParser(this.schemaRouter);
+      } catch (final IOException e) {
+        promise.fail(e);
       }
+      promise.complete();
     });
   }
 }
--- src/test/java/com/notessensei/demo/jsonmystery/TestMainVerticle.java
+++ src/test/java/com/notessensei/demo/jsonmystery/TestMainVerticle.java
@@ -1,22 +1,139 @@
 package com.notessensei.demo.jsonmystery;
 
-import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import com.google.common.io.Resources;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
   @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  void deploy_verticle(final Vertx vertx, final VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(),
+        testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  JsonObject getJson(final String resourceName) {
+    final URL url = this.getClass().getResource(resourceName);
+    String openAPIString;
+    try {
+      openAPIString = Resources.toString(url, StandardCharsets.UTF_8);
+      return new JsonObject(openAPIString);
+    } catch (final Exception e) {
+      e.printStackTrace();
+    }
+    return new JsonObject();
   }
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+  void send_street_invalid(final Vertx vertx, final VertxTestContext testContext) throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/street_bad.json");
+    client
+        .post(8888, "localhost", "/street")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          Assertions.assertEquals("provided object should contain property name", result);
+          testContext.completeNow();
+        });
+  }
+
+  @Test
+  void send_street_invalid_complete(final Vertx vertx, final VertxTestContext testContext)
+      throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/street_bad_complete.json");
+    client
+        .post(8888, "localhost", "/street")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          System.out.println(result);
+          Assertions.assertEquals(500, response.statusCode());
+          testContext.completeNow();
+        });
+  }
+
+  @Test
+  void send_street_valid(final Vertx vertx, final VertxTestContext testContext) throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/street_good.json");
+    client
+        .post(8888, "localhost", "/street")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          Assertions.assertEquals("The submitted road is fine!", result);
+          testContext.completeNow();
+        });
+  }
+
+  @Test
+  void send_street_valid_complete(final Vertx vertx, final VertxTestContext testContext)
+      throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/street_good_complete.json");
+    client
+        .post(8888, "localhost", "/street")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          System.out.println(result);
+          Assertions.assertEquals(200, response.statusCode());
+          testContext.completeNow();
+        });
+  }
+
+  @Test
+  void send_traffic_light_invalid(final Vertx vertx, final VertxTestContext testContext)
+      throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/trafficwrong.json");
+    client
+        .post(8888, "localhost", "/trafficlight")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          Assertions
+              .assertTrue(result.startsWith("Input doesn't match one of allowed values of enum:"));
+          testContext.completeNow();
+        });
+  }
+
+  @Test
+  void send_traffic_light_valid(final Vertx vertx, final VertxTestContext testContext)
+      throws Throwable {
+    final WebClient client = WebClient.create(vertx);
+    final JsonObject body = this.getJson("/trafficlight.json");
+    client
+        .post(8888, "localhost", "/trafficlight")
+        .sendJson(body)
+        .onFailure(err -> testContext.failNow(err))
+        .onSuccess(response -> {
+          final String result = response.bodyAsString();
+          Assertions.assertEquals("The submitted traffic light is complete!", result);
+          testContext.completeNow();
+        });
+  }
+
+
+  @Test
+  void verticle_deployed(final Vertx vertx, final VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
   }
 }
